bf18fd29bf74e9836ea13d063008d5694158bfee
Asghar Ibraheem Baloch <46741864+asgharibraheembaloch@users.noreply.github.com>
2020-06-15 11:43:02 +0500

    update gitignore
    build is out from ignore

---
+++ build/precache-manifest.217f9c789f9e463b0a8a0209fa910314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d470ea9b35992592904e33d0c6f5ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868c9ee8e935609f8bd4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "836a6db001e7ecac70fc",
+    "url": "/static/js/2.cd03877a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd03877a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868c9ee8e935609f8bd4",
+    "url": "/static/js/main.5c78f280.chunk.js"
+  },
+  {
+    "revision": "1d36e03179663ac3ec4d",
+    "url": "/static/js/runtime-main.c92a3cba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
